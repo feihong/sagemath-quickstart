4e77385ba3445d3818f34ac72fca08df2368ee2a
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-04 15:31:11 -0600

    Add sind, cosd, and tand helper functions

--- util.py
+++ util.py
@@ -1,13 +1,16 @@
+import functools
 from sage.misc.functional import n
 from sage.symbolic.constants import pi
 from sage.misc.decorators import infix_operator
+from sage.functions.trig import sin, cos, tan
+
 
 def fmt(v):
     return f'{n(v):0,.2f}'
 
 def printnum(v):
     print(fmt(v))
-    
+
 def radians(degrees):
     return degrees * pi/180
 
@@ -17,3 +20,17 @@ def degrees(radians):
 @infix_operator('or')
 def pipe(a, b):
     return b(a)
+
+def use_degrees(f):
+    """
+    Wrap a one-argument function so that it converts the argument from degrees to radians before applying
+    """
+    @functools.wraps(f)
+    def wrapped_function(v):
+        return f(radians(v))
+
+    return wrapped_function
+
+sind = use_degrees(sin)
+cosd = use_degrees(cos)
+tand = use_degrees(tan)
